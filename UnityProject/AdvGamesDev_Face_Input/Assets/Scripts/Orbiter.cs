ec4290ce2f7cdbd2049a0709bd0fd18bc0374eea
OliverSheard <55920246+OliverSheard@users.noreply.github.com>
2020-02-10 12:58:32 +0000

    Initial commit
    Looking into creating custom integration for OpenCV, focusing on the Face Detection / Tracking applications

---
+++ UnityProject/AdvGamesDev_Face_Input/Assets/Scripts/Orbiter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour
+{
+    float range;
+    [SerializeField] private float orbitSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        range = transform.localPosition.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
