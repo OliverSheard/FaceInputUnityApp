965ed2aa0ca0c85abd76be85d97256214c31b8e2
Oliver Sheard <oliver.sheard@hotmail.co.uk>
2020-02-12 11:15:51 +0000

    Core movement input
    Reviewing a .NET wrapper for OpenCV to use for face tracking

--- UnityProject/AdvGamesDev_Face_Input/Assets/Scripts/Orbiter.cs
+++ UnityProject/AdvGamesDev_Face_Input/Assets/Scripts/Orbiter.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Orbiter : MonoBehaviour
-{
-    float range;
-    [SerializeField] private float orbitSpeed = 1f;
+{    
+    [SerializeField] private Vector3 orbitSpeed = new Vector3(1,1,1);
+    [SerializeField] private bool xRotation, yRotation, zRotation;
+    private Quaternion baseRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        range = transform.localPosition.magnitude;
+        baseRotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var x = xRotation ? orbitSpeed.x * Time.deltaTime : 0;
+        var y = yRotation ? orbitSpeed.y * Time.deltaTime : 0;
+        var z = zRotation ? orbitSpeed.z * Time.deltaTime : 0;
+
+        transform.Rotate(x, y, z);
     }
 }
---
+++ UnityProject/AdvGamesDev_Face_Input/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Vector2 playerSpeed = new Vector2(1,1);
+    [SerializeField] private GameObject spaceRotation;
+    private Quaternion baseRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseRotation = transform.rotation;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var forward = Input.GetAxis("Vertical") * playerSpeed.x * Time.deltaTime;
+        var turning = Input.GetAxis("Horizontal") * playerSpeed.y * Time.deltaTime;
+
+        spaceRotation.transform.Rotate(-forward, -turning, 0, Space.World);
+
+        transform.rotation = Quaternion.Euler(baseRotation.eulerAngles + new Vector3(Mathf.Cos(Time.fixedTime * 5) * 3, Mathf.Sin(Time.fixedTime * 13) * 7, Mathf.Sin(Time.fixedTime * 19) * 4));
+
+    }
+}
