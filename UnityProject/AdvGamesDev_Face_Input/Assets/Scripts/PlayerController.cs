5816899775860dbe8b17637acd266ca0b1379425
OliverSheard <55920246+OliverSheard@users.noreply.github.com>
2020-03-02 12:41:08 +0000

    Attacking in Unityproject

--- UnityProject/AdvGamesDev_Face_Input/Assets/Scripts/PlayerController.cs
+++ UnityProject/AdvGamesDev_Face_Input/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Vector2 playerSpeed = new Vector2(1,1);
-    [SerializeField] private GameObject spaceRotation;
+    [SerializeField] private Vector2 attackSpeed, moveSpeed = new Vector2(1,1);
+    [SerializeField] private GameObject spaceRotation, attackVFX;
+    [SerializeField] private float fireThreshold;
+    private Vector2 playerSpeed;
     private Quaternion baseRotation;
     // Start is called before the first frame update
     void Start()
@@ -18,11 +18,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        var attacking = Attacking();
+        Move(attacking);
+        Attack(attacking);
     }
 
-    private void Move()
+    private void Move(bool attacking)
     {
+        playerSpeed = attacking ? attackSpeed : moveSpeed;
         var forward = Input.GetAxis("Vertical") * playerSpeed.x * Time.deltaTime;
         var turning = Input.GetAxis("Horizontal") * playerSpeed.y * Time.deltaTime;
 
@@ -31,6 +34,29 @@ public class PlayerController : MonoBehaviour
         //Creates the wobble effect on the UFO
         transform.rotation = Quaternion.Euler(baseRotation.eulerAngles + new Vector3(Mathf.Cos(Time.fixedTime * 5) * 3 + forward, Mathf.Sin(Time.fixedTime * 13) * 7, Mathf.Sin(Time.fixedTime * 19) * 4 + turning));
 
+    }
+
+    private void Attack(bool attacking)
+    {
+        attackVFX.SetActive(attacking);
+        if (attacking)
+        {
+            //Fire button activated
+            var ray = new Ray(transform.position, Vector3.down);
+            RaycastHit[] hitUnits = Physics.RaycastAll(ray, 10f);
+
+            foreach (var hit in hitUnits)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+
+            Debug.DrawRay(transform.position, Vector3.down, Color.red, 1f);
+        }
+    }
+
+    public bool Attacking()
+    {
+        return Mathf.Abs(Input.GetAxis("Fire1")) > fireThreshold;
     }
 }
 
